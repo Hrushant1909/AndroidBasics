e07e44cbe02b8bdc3ca579337f912ef86fc11f91
Hrushant <hsmotinge04@gmail.com>
2026-08-05 12:49:27 +0530

    Spinner implemented

--- app/src/main/java/com/hrushant/basicsofandroid/MainActivity.kt
+++ app/src/main/java/com/hrushant/basicsofandroid/MainActivity.kt
@@ -9,12 +9,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -31,56 +34,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn_db_1 = findViewById<Button>(R.id.btn_db1)
-        val btn_db_2 = findViewById<Button>(R.id.btn_db2)
-        val btn_db_3 = findViewById<Button>(R.id.btn_db3)
-        val addContactDialog = AlertDialog.Builder(this)
-            .setTitle("Add Contact")
-            .setMessage("Do you want to add Mr. Hrushant to contact list ?")
-            .setIcon(R.drawable.ic_add_contact)
-            .setPositiveButton("Yes") {_, _ ->
-                Toast.makeText(this, "Mr. Hrushant contact added..", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("No") {_, _ ->
-                Toast.makeText(this, "Mr. Hrushant was not added in contact..", Toast.LENGTH_SHORT).show()
-            }.create()
-        btn_db_1.setOnClickListener {
-            addContactDialog.show()
-        }
 
-        val options = arrayOf("First item", "Second item", "thire item")
-        var singleChoiceDialog = AlertDialog.Builder(this)
-            .setTitle("Choose one  of these options..")
-            .setSingleChoiceItems(options, 0) {_, i ->
-                Toast.makeText(this, "You clicked on ${options[i]}", Toast.LENGTH_SHORT).show()
+
+
+        val spMonths = findViewById<Spinner>(R.id.sp_months)
+        spMonths.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity, "Your selected : ${adapterView?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_SHORT).show()
             }
-            .setPositiveButton("Accept") {_, _ ->
-                Toast.makeText(this, "You accepted!!", Toast.LENGTH_SHORT).show()
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
             }
-            .setNegativeButton("Decline"){_, _ ->
-                Toast.makeText(this, "You declined!!", Toast.LENGTH_SHORT).show()
-            }
-        btn_db_2.setOnClickListener {
-            singleChoiceDialog.show()
         }
 
 
-        var multiChoiceDialog = AlertDialog.Builder(this)
-            .setTitle("Choose one  of these options..")
-            .setMultiChoiceItems(options, booleanArrayOf(false, false, false)){ _, i , b->
-                if(b)
-                    Toast.makeText(this, "You checked options ${options[i]}", Toast.LENGTH_SHORT).show()
-                else
-                    Toast.makeText(this, "You unchecked options ${options[i]}", Toast.LENGTH_SHORT).show()
-            }
-            .setPositiveButton("Accept") {_, _ ->
-                Toast.makeText(this, "You accepted!!", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("Decline"){_, _ ->
-                Toast.makeText(this, "You declined!!", Toast.LENGTH_SHORT).show()
-            }
-        btn_db_3.setOnClickListener {
-            multiChoiceDialog.show()
-        }
+
     }
 }
