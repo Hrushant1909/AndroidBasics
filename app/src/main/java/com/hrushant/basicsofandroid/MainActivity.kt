11cdc16725069e39c9263509335775c00426b3aa
Hrushant <hsmotinge04@gmail.com>
2026-08-05 12:34:54 +0530

    Alery doalog boxes implemented

--- app/src/main/java/com/hrushant/basicsofandroid/MainActivity.kt
+++ app/src/main/java/com/hrushant/basicsofandroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hrushant.basicsofandroid
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -17,6 +18,7 @@ import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
@@ -29,30 +31,56 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.appbar_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.mi_settings -> {
-                Toast.makeText(this, "You clicked on settings...", Toast.LENGTH_SHORT).show()
+        val btn_db_1 = findViewById<Button>(R.id.btn_db1)
+        val btn_db_2 = findViewById<Button>(R.id.btn_db2)
+        val btn_db_3 = findViewById<Button>(R.id.btn_db3)
+        val addContactDialog = AlertDialog.Builder(this)
+            .setTitle("Add Contact")
+            .setMessage("Do you want to add Mr. Hrushant to contact list ?")
+            .setIcon(R.drawable.ic_add_contact)
+            .setPositiveButton("Yes") {_, _ ->
+                Toast.makeText(this, "Mr. Hrushant contact added..", Toast.LENGTH_SHORT).show()
             }
-            R.id.mi_peoples -> {
-                Toast.makeText(this, "You clicked on peoples...", Toast.LENGTH_SHORT).show()
-            }
-            R.id.mi_favourite -> {
-                Toast.makeText(this, "You clicked on favourite...", Toast.LENGTH_SHORT).show()
-            }
-            R.id.mi_add_contact -> {
-                Toast.makeText(this, "You clicked on add contact...", Toast.LENGTH_SHORT).show()
-            }
-
+            .setNegativeButton("No") {_, _ ->
+                Toast.makeText(this, "Mr. Hrushant was not added in contact..", Toast.LENGTH_SHORT).show()
+            }.create()
+        btn_db_1.setOnClickListener {
+            addContactDialog.show()
         }
-        return true
+
+        val options = arrayOf("First item", "Second item", "thire item")
+        var singleChoiceDialog = AlertDialog.Builder(this)
+            .setTitle("Choose one  of these options..")
+            .setSingleChoiceItems(options, 0) {_, i ->
+                Toast.makeText(this, "You clicked on ${options[i]}", Toast.LENGTH_SHORT).show()
+            }
+            .setPositiveButton("Accept") {_, _ ->
+                Toast.makeText(this, "You accepted!!", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Decline"){_, _ ->
+                Toast.makeText(this, "You declined!!", Toast.LENGTH_SHORT).show()
+            }
+        btn_db_2.setOnClickListener {
+            singleChoiceDialog.show()
+        }
+
+
+        var multiChoiceDialog = AlertDialog.Builder(this)
+            .setTitle("Choose one  of these options..")
+            .setMultiChoiceItems(options, booleanArrayOf(false, false, false)){ _, i , b->
+                if(b)
+                    Toast.makeText(this, "You checked options ${options[i]}", Toast.LENGTH_SHORT).show()
+                else
+                    Toast.makeText(this, "You unchecked options ${options[i]}", Toast.LENGTH_SHORT).show()
+            }
+            .setPositiveButton("Accept") {_, _ ->
+                Toast.makeText(this, "You accepted!!", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Decline"){_, _ ->
+                Toast.makeText(this, "You declined!!", Toast.LENGTH_SHORT).show()
+            }
+        btn_db_3.setOnClickListener {
+            multiChoiceDialog.show()
+        }
     }
 }
